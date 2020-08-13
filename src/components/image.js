bd249d3512c88bc91f8f769134ded166d61c098f
Diego <diegosantaolallamartin@gmail.com>
2020-08-13 13:10:42 +0200

    Added eslint to enforce code styles

--- src/components/image.js
+++ src/components/image.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StaticQuery, graphql } from 'gatsby';
 import Img from 'gatsby-image';
+import PropTypes from 'prop-types';
 
 // Note: You can change "images" to whatever you'd like.
 
-const Image = props => (
+const Image = (props) => (
   <StaticQuery
     query={graphql`
       query {
@@ -23,18 +24,21 @@ const Image = props => (
         }
       }
     `}
-    render={data => {
-      const image = data.images.edges.find(n => {
-        return n.node.relativePath.includes(props.filename);
-      });
+    render={(data) => {
+      const image = data.images.edges.find((n) => n.node.relativePath.includes(props.filename));
       if (!image) {
         return null;
       }
 
-      //const imageSizes = image.node.childImageSharp.sizes; sizes={imageSizes}
+      // const imageSizes = image.node.childImageSharp.sizes; sizes={imageSizes}
       return <Img alt={props.alt} fluid={image.node.childImageSharp.fluid} />;
     }}
   />
 );
+
+Image.propTypes = {
+  filename: PropTypes.string,
+  alt: PropTypes.string,
+};
 
 export default Image;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,19 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import { Link } from 'gatsby';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
+import Image from '../components/image';
+import SEO from '../components/seo';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image filename={'gatsby-astronaut.png'} alt={'gatsby-astronaut.png'} />
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <Link to="/using-typescript/">Go to Using TypeScript</Link>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
