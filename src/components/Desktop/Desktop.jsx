61e8af062d903f06f60b3237c8920a43bba1349f
Diego <diegosantaolallamartin@gmail.com>
2020-08-14 02:31:17 +0200

    Created desktop icons

--- src/components/Desktop/Desktop.jsx
+++ src/components/Desktop/Desktop.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import styles from './Desktop.module.scss';
+import DesktopIcon from './DesktopIcon/DesktopIcon.jsx';
+import iconList from '../../data/iconList';
 
 const Desktop = () => (
   <div className={`desktop ${styles.desktop}`}>
-    Desktop
+    <DesktopIconList />
   </div>
 );
+
+const getDesktopIconList = () => {
+  const { i18n } = useTranslation('common');
+  return (iconList(i18n.language));
+};
+
+const DesktopIconList = () => {
+  const desktopIconList = getDesktopIconList();
+  const render = [];
+
+  if (desktopIconList) {
+    desktopIconList.map((value, index) => render.push(
+      <DesktopIcon
+        key={index}
+        image={value.image}
+        label={value.label}
+        value={value.value}
+      />,
+    ));
+  }
+
+  return (
+    <div>
+      {render}
+    </div>
+  );
+};
 
 export default Desktop;
